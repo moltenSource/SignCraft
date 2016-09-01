88a352b7ce0d6b3bc075640e4d6c58431e1fd9fa
Daniel Hottmeyer <daniel@hottmeyer.de>
2016-09-01 12:05:33 +0200

    * Added basic core file * Added mod informations * Updated gradle build, now we only need to change versions at one file (thanks to OpenComputers)

---
+++ src/main/java/de/moltensource/SignCraft.java
@@ -0,0 +1,17 @@
+package de.moltensource;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = SignCraft.MODID, name = SignCraft.MODID, version = SignCraft.VERSION)
+public class SignCraft {
+
+    public static final String MODID = "SignCraft";
+    public static final String VERSION = "@VERSION@";
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        // TODO: Do some work here
+    }
+}
